94ee5a55611eb70bbda2d7c9506a2535108179ac
Robert Albus <hello@robertalbus.com>
2022-07-23 23:09:30 -0600

    add global constants to Arity for min and max arity values and error handling

--- src/graph/arity.h
+++ src/graph/arity.h
@@ -1,5 +1,12 @@
 #pragma once
-#include<algorithm>
+
+#ifndef MAX_ARITY
+#define MAX_ARITY 2
+#endif
+
+#ifndef MIN_ARITY
+#define MIN_ARITY 0
+#endif
 
 /// A base class for objects with channal arity.
 /// It may be useful to cast a Node with unknown arity to this class to read it's arity;
@@ -9,7 +16,11 @@ public:
     Arity() {
         throw std::domain_error("must instantiate an Arity with a value");
     };
-    Arity(int _arity) : _arity(_arity) {};
+    Arity(int _arity) : _arity(_arity) {
+        if (MIN_ARITY > _arity || _arity > MAX_ARITY ) {
+            throw std::domain_error("Arity(int) must be instantiated with a value between MIN_ARITY and MAX_ARITY. See arity.h");
+        }
+    };
     int arity() const {
         return _arity;
     }
