fad2741b6dfcc486c7fd936009c4acca23615d1f
Robert Albus <hello@robertalbus.com>
2022-07-24 13:33:34 -0600

    add freq and phase controls to Sine

--- src/sine/sine.cpp
+++ src/sine/sine.cpp
@@ -6,26 +6,59 @@
 
 #define TABLE_SIZE   (200)
 
-Sine::Sine() : Node(0,2), phase(0)
+Sine::Sine() : Node(0,2)
 {
+    freq(100);
+    phase(0);
     /* initialise sinusoidal wavetable */
     for( int i=0; i<TABLE_SIZE; i++ )
     {
-        sine[i] = (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. );
+        wavetable[i] = (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. );
     }
 }
 
 Frame Sine::tick(Frame input)
 {
-    Sample value = sine[(int)phase++];
+    Sample value = lerp();
 
-    if (phase >= TABLE_SIZE) {
-        phase -= TABLE_SIZE;
-    }
+    phase(_phase + _phaseIncrement);
 
     Frame f = Frame(2);
     f.setSampleAtIndex(0, value);
     f.setSampleAtIndex(1, value);
 
     return f;
+}
+
+void Sine::freq(float freq) {
+    _phaseIncrement = freq * ((float)TABLE_SIZE) / ((float)SAMPLE_RATE);
+}
+
+float Sine::freq() {
+    return _readonlyFreq;
+}
+
+void Sine::phase(float p) {
+    while (p >= static_cast<float>(TABLE_SIZE)) {
+        p -= static_cast<float>(TABLE_SIZE);
+    }
+    _phase = p;
+}
+
+float Sine::phase() {
+    return _phase;
+}
+
+float Sine::lerp() {
+    int truncatedIndex = static_cast<int>(_phase);
+    int nextIndex = truncatedIndex + 1;
+    if (nextIndex >= TABLE_SIZE) {
+        nextIndex -= TABLE_SIZE;
+    }
+
+    float nextIndexWeight = _phase - static_cast<float>(truncatedIndex);
+    float truncatedIndexWeight = 1. - nextIndexWeight;
+
+    return truncatedIndexWeight * wavetable[truncatedIndex] + 
+        nextIndexWeight * wavetable[nextIndex];
 }
--- src/sine/sine.h
+++ src/sine/sine.h
@@ -7,10 +7,27 @@ class Sine : public Node
 public:
     Sine();
 
+    /// Set frequency
+    void freq(float freq);
+
+    /// Get frequency
+    float freq();
+
+    /// set phase
+    void phase(float phase);
+    
+    // Get phase
+    float phase();
+
 protected:
     Frame tick(Frame input);
 
-    Sample sine[TABLE_SIZE];
-    int phase;
+    float lerp();
+
+    Sample wavetable[TABLE_SIZE];
+private:
+    float _phase;
+    float _phaseIncrement;
+    float _readonlyFreq;
 };
 
