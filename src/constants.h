c035b02522b1602ce736ae4fae23e8bd8a48a79a
Robert Albus <hello@robertalbus.com>
2022-08-07 21:09:33 -0600

    remove unneeded constexpr

--- src/constants.h
+++ src/constants.h
@@ -2,8 +2,6 @@
 #include <cstddef>
 
 constexpr size_t SAMPLE_RATE = 48000;
-constexpr size_t MAX_ARITY = 2;
-constexpr size_t MIN_ARITY = 0;
 constexpr double PI = 3.14159265;
 constexpr bool DEBUG_PRINT = false;
 constexpr size_t DEFAULT_WAVETABLE_SIZE = 200;
