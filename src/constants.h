9bb15f099c7c09c02bf2c40aa73098795d736cf3
Robert Albus <hello@robertalbus.com>
2022-08-08 18:12:39 -0600

    reduce node max capacity to avoid running extensive load testing

--- src/constants.h
+++ src/constants.h
@@ -5,6 +5,7 @@ constexpr size_t SAMPLE_RATE = 48000;
 constexpr double PI = 3.14159265;
 constexpr bool DEBUG_PRINT = false;
 constexpr size_t DEFAULT_WAVETABLE_SIZE = 200;
-constexpr size_t NODE_MAX_INPUT_CAPACITY = 1650;
+// constexpr size_t NODE_MAX_INPUT_CAPACITY = 1650;
+constexpr size_t NODE_MAX_INPUT_CAPACITY = 10;
 
 typedef float Sample;
