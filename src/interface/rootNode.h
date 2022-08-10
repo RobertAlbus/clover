5549bcffada69f8aeaede6a7ebd2c84866458a0f
Robert Albus <hello@robertalbus.com>
2022-08-09 23:27:41 -0600

    make RootNode's tick method protected

--- src/interface/rootNode.h
+++ src/interface/rootNode.h
@@ -1,18 +1,17 @@
 #pragma once
-#include "gain.h"
+#include "node.h"
 
 template <size_t __arity>
 class RootNode : public Node<__arity,__arity>
 {
 public:
-
-    Frame<__arity> tick(Frame<__arity> input) 
-    {
-        return input;
-    }
-
     /// RootNode exposes _tick publicly as the starting point of the signal graph
     void _tick(int currentTime) {
         Node<__arity,__arity>::_tick(currentTime);
+    }
+protected:
+    Frame<__arity> tick(Frame<__arity> input) 
+    {
+        return input;
     }
 };
