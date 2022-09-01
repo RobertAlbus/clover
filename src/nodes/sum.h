e330033a61bdca477ad47ba74b30845d1e1eddad
Robert Albus <hello@robertalbus.com>
2022-09-01 12:01:29 -0600

    lint whitespace

--- src/nodes/sum.h
+++ src/nodes/sum.h
@@ -4,16 +4,16 @@
 
 auto sum1Fn = [](Frame<2> input)
 {
-        return Frame<2> {(input[0] + input[1]) * 0.5f};
+  return Frame<2> {(input[0] + input[1]) * 0.5f };
 };
 class Sum1 : public StatelessProcessor<2,2,sum1Fn> { };
 
 auto sum2Fn = [](Frame<2> input)
 {
-        return Frame<2>
-        {
-            (input[0] + input[1]) * 0.5f,
-            (input[0] + input[1]) * 0.5f
-        };
+  return Frame<2>
+  {
+    (input[0] + input[1]) * 0.5f,
+    (input[0] + input[1]) * 0.5f
+  };
 };
 class Sum2 : public StatelessProcessor<2,2,sum2Fn> { };
