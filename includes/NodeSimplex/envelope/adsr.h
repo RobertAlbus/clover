b731db7c3ab5fdb7eed7989bd66816c4cbaaf917
Robert Albus <hello@robertalbus.com>
2022-11-20 19:49:48 -0700

    update Adsr envelope to use the NodeSettings pattern

--- includes/NodeSimplex/envelope/adsr.h
+++ includes/NodeSimplex/envelope/adsr.h
@@ -5,94 +5,85 @@
 #include "NodeSimplex.h"
 #include "graph.h"
 
-using namespace Clover::Graph;
 
 namespace Clover::NodeSimplex::Envelope {
 
 struct AdsrSettings
 {
-    size_t attack;
-    size_t decay;
-    float  sustain;
-    size_t release;
-    size_t startTime;
-    bool   keyOn;
+  AdsrSettings() : AdsrSettings(0.,0.,1.,0.) { }
+  AdsrSettings(size_t a, size_t d, float s, size_t r)
+  : attack(a), decay(d), sustain(s), release(r),
+  startTime(0), keyOn(false) {}
+
+  size_t attack;
+  size_t decay;
+  float  sustain;
+  size_t release;
+  size_t startTime;
+  bool   keyOn;
 };
 
 class Adsr : public StatefulSubgraph<0,1, AdsrSettings>
 {
 public:
-    Adsr() : Adsr(0.,0.,1.,0.) { }
+  Adsr(AdsrSettings initialSettings) : StatefulSubgraph(initialSettings), envelope(0,0,0)
+  {
+    connectNodes();
+    set(
+      settings.initial.attack,
+      settings.initial.decay,
+      settings.initial.sustain,
+      settings.initial.attack
+    );
+  }
 
-    Adsr(size_t a, size_t d, float s, size_t r) :
-        StatefulSubgraph(), envelope(0,0,1)
-    {
-        settings.initial.attack  = a;
-        settings.initial.decay   = d;
-        settings.initial.sustain = s;
-        settings.initial.release = r;
-        settings.initial.keyOn = false;
-        settings.initial.startTime = 0;
+  void set(size_t a, size_t d, float s, size_t r)
+  {
+    attack(a);
+    decay(d);
+    sustain(s);
+    release(r);
+  }
 
-        settings.reset();
+  size_t attack()        { return settings.current.attack; }
+  void attack(size_t a)  { settings.current.attack = a; }
 
-        connectNodes();
-        set(a, d, s, r);
-    }
+  size_t decay()         { return settings.current.decay; }
+  void decay(size_t d)   { settings.current.decay = d; }
 
-    void set(size_t a, size_t d, float s, size_t r)
-    {
-        attack(a);
-        decay(d);
-        sustain(s);
-        release(r);
-    }
+  float sustain()        { return settings.current.sustain; }
+  void sustain(float s)  { settings.current.sustain = s; }
 
-    size_t attack()        { return settings.current.attack; }
-    void attack(size_t a)  { settings.current.attack = a; }
+  size_t release()       { return settings.current.release; }
+  void release(size_t r) { settings.current.release = r; }
 
-    size_t decay()         { return settings.current.decay; }
-    void decay(size_t d)   { settings.current.decay = d; }
+  void keyOn()  { settings.current.keyOn = true;  settings.current.startTime = _currentClockTime + 1; }
+  void keyOff() { settings.current.keyOn = false; settings.current.startTime = _currentClockTime + 1; }
 
-    float sustain()        { return settings.current.sustain; }
-    void sustain(float s)  { settings.current.sustain = s; }
-
-    size_t release()       { return settings.current.release; }
-    void release(size_t r) { settings.current.release = r; }
-
-    void keyOn()  { settings.current.keyOn = true;  settings.current.startTime = _currentClockTime + 1; }
-    void keyOff() { settings.current.keyOn = false; settings.current.startTime = _currentClockTime + 1; }
-  
 protected:
-    BasicEnvelope envelope;
+  BasicEnvelope envelope;
 
-    Frame<1> tick(Frame<0> input)
-    {
-        AdsrSettings& s = settings.current;
+  Frame<1> tick(Frame<0> input)
+  {
+    AdsrSettings& s = settings.current;
 
-        bool isStartOfSustain =  s.keyOn && _currentClockTime >  s.startTime + s.attack + s.decay;
-        bool isStartOfDecay   =  s.keyOn && _currentClockTime == s.startTime + s.attack;
-        bool isStartOfAttack  =  s.keyOn && _currentClockTime == s.startTime;
-        bool isStartOfRelease = !s.keyOn && _currentClockTime == s.startTime;
-        printf("\n%i %i %i %i    |    %i    %i %i %f %i    |    %i",
-            isStartOfAttack, isStartOfDecay, isStartOfSustain, isStartOfRelease,
-            s.keyOn, s.attack, s.decay, s.sustain, s.release, s.startTime
-            
-            
-        );
-        
-        if      ( isStartOfSustain ) { envelope.set( s.sustain,         s.sustain,  0        ); }
-        else if ( isStartOfDecay   ) { envelope.set( 1.,                s.sustain,  s.decay  ); }
-        else if ( isStartOfAttack  ) { envelope.set( 0.,                1.,         s.attack ); }
-        else if ( isStartOfRelease ) { envelope.set( frames.current[0], 0,          s.release); }
+    bool isStartOfSustain =  s.keyOn && _currentClockTime >  s.startTime + s.attack + s.decay;
+    bool isStartOfDecay   =  s.keyOn && _currentClockTime == s.startTime + s.attack;
+    bool isStartOfAttack  =  s.keyOn && _currentClockTime == s.startTime;
+    bool isStartOfRelease = !s.keyOn && _currentClockTime == s.startTime;
+    
+    if      ( isStartOfSustain ) { envelope.set( s.sustain,         s.sustain,  0        ); }
+    else if ( isStartOfDecay   ) { envelope.set( 1.,                s.sustain,  s.decay  ); }
+    else if ( isStartOfAttack  ) { envelope.set( 0.,                1.,         s.attack ); }
+    else if ( isStartOfRelease ) { envelope.set( frames.current[0], 0,          s.release); }
 
-        return blackHole.frames.current;
-    }
- 
-    void connectNodes()
-    {
-        envelope >> blackHole;
-    }
+    return blackHole.frames.current;
+  }
+
+  void connectNodes()
+  {
+    envelope >> blackHole;
+  }
 };
 
 }
