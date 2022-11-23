0de3a628797d7e7ed3d7a64ffc6fc9fd4e49af21
Robert Albus <hello@robertalbus.com>
2022-11-22 18:54:06 -0700

    upgrade DC envelope to use NodeSettings pattern

--- includes/NodeSimplex/envelope/dc.h
+++ includes/NodeSimplex/envelope/dc.h
@@ -4,24 +4,34 @@
 
 namespace Clover::NodeSimplex::Envelope {
 
+struct DCSettings
+{
+  DCSettings() : DCSettings(0) {}
+  DCSettings(Sample value) :value(value) {}
+
+  Sample value;
+};
+
 template <size_t __arity>
-class DC : public Node<1, 1>
+class DC : public StatefulProcessor<1, 1, DCSettings>
 {
 public:
-  DC(Sample value) : Node<__arity, __arity>(), _value(value) {}
+  DC() : DC(0) {}
+  DC(Sample value) : DC(*(new DCSettings(value))) {}
+  DC(DCSettings initialSettings)
+  : StatefulProcessor(initialSettings) {}
 
-  void   value(Sample v) { _value = v; }
-  Sample value()         { return _value; }
+  void   value(Sample v) { settings.current.value = v; }
+  Sample value()         { return settings.current.value; }
 
 protected:
-  Sample _value;
   Frame<__arity> tick(Frame<__arity> input)
   {
     Frame<__arity> f {};
 
     for (int i = 0; i < __arity; i++)
     {
-      f[i] = _value + input[i];
+      f[i] = settings.current.value + input[i];
     }
 
     return f;
