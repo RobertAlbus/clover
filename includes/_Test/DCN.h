af73d509c238bf5915a8ac951e7084e04d3ee11f
Robert Albus <hello@robertalbus.com>
2023-01-03 16:57:57 -0700

    add an index-basis to the DCN testing fixture to allow customization of the test signal

--- includes/_Test/DCN.h
+++ includes/_Test/DCN.h
@@ -9,14 +9,17 @@ namespace Clover::_Test {
 
 template <size_t __arityOut> class DCN : public Node<0, __arityOut> {
 public:
-  DCN() : Node<0, __arityOut>() {}
+  DCN() : Node<0, __arityOut>(), basis(0.f) {}
+
+  void indexBasis(float b) { basis = b; }
 
 private:
+  float basis;
   Frame<__arityOut> tick(Frame<0> input) {
     Frame<__arityOut> f{};
 
     for (int i = 0; i < __arityOut; i++) {
-      f[i] = i;
+      f[i] = i + basis;
     }
 
     return f;
