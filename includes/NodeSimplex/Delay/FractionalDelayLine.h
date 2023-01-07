43c8c9c7bc9fd8699eafdc10a07917a8ec214ea2
Robert Albus <hello@robertalbus.com>
2023-01-07 16:22:55 -0700

    improve the API of FractionalDelayLine

--- includes/NodeSimplex/Delay/FractionalDelayLine.h
+++ includes/NodeSimplex/Delay/FractionalDelayLine.h
@@ -9,43 +9,58 @@
 namespace Clover::NodeSimplex::Delay {
 
 struct FractionalDelayLineSettings {
-  FractionalDelayLineSettings() : FractionalDelayLineSettings(0) {}
-  FractionalDelayLineSettings(float delayTime) : delayTime(delayTime) {}
   float delayTime;
+  int bufferCapacity;
 };
 
-template <size_t __arity, size_t __bufferSize>
+template <size_t __arity>
 class FractionalDelayLine
     : public StatefulProcessor<__arity, __arity, FractionalDelayLineSettings> {
 public:
+  FractionalDelayLine(int bufferCapacity, float delaySamples)
+      : StatefulProcessor<__arity, __arity, FractionalDelayLineSettings>(),
+        buffer{}, writeHead(0), readHead(0) {
+
+    this->settings.initial.delayTime = delaySamples;
+    this->settings.initial.bufferCapacity = bufferCapacity;
+    this->settings.reset();
+
+    buffer.reserve(bufferCapacity);
+    delay(delaySamples);
+  }
+
   FractionalDelayLine(FractionalDelayLineSettings initialSettings)
       : StatefulProcessor<__arity, __arity, FractionalDelayLineSettings>(
             initialSettings),
         buffer{}, writeHead(0), readHead(0) {
-    buffer.reserve(__bufferSize);
-    setDelayTime(initialSettings.delayTime);
+    buffer.reserve(initialSettings.delayTime);
+    delay(initialSettings.delayTime);
+
+    this->settings.reset();
   }
 
-  void setDelayTime(float time) {
-    this->settings.current.delayTime = time;
-
-    float bufferSize = (float)__bufferSize;
+  void delay(float time) {
+    float bufferSize = (float)this->settings.current.bufferCapacity;
     time = std::clamp(time, 0.f, bufferSize - 1.f);
+    this->settings.current.delayTime = time;
 
     readHead = fmod(((float)writeHead - time + bufferSize), bufferSize);
   }
+  float delay() { return this->settings.current.delayTime; }
 
 protected:
   std::vector<Frame<__arity>> buffer;
   float readHead;
   int writeHead;
   Frame<__arity> tick(Frame<__arity> input) {
+    int bufferCapacity = this->settings.current.bufferCapacity;
+
     // push new data to buffer
     buffer[writeHead] = input;
 
-    // linear interpolate between current and  next frame
+    // lerp
     int previousReadHead = (int)readHead;
-    int nextReadHead = (previousReadHead + 1) % __bufferSize;
+    int nextReadHead = (previousReadHead + 1) % bufferCapacity;
     float lerpAmount = readHead - (float)previousReadHead;
 
     Frame<__arity> f{};
@@ -55,8 +70,8 @@ protected:
     }
 
     // advance the buffer
-    readHead = fmod(readHead + 1.f, 1.0 * __bufferSize);
-    writeHead = (writeHead + 1) % __bufferSize;
+    readHead = fmod(readHead + 1.f, (float)bufferCapacity);
+    writeHead = (writeHead + 1) % bufferCapacity;
 
     return f;
   }
