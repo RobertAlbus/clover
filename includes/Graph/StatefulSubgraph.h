8b7429e03efb652b257e76063ff5bbe35090dc6c
Robert Albus <hello@robertalbus.com>
2023-01-07 12:23:07 -0700

    add parameterless ctor to the StatefulSubgraph base class

--- includes/Graph/StatefulSubgraph.h
+++ includes/Graph/StatefulSubgraph.h
@@ -9,6 +9,9 @@ template <size_t __arityIn, size_t __arityOut, class SettingsType>
 class StatefulSubgraph
     : public StatefulProcessor<__arityIn, __arityOut, SettingsType> {
 public:
+  StatefulSubgraph()
+      : StatefulProcessor<__arityIn, __arityOut, SettingsType>() {}
+
   StatefulSubgraph(SettingsType initialSettings)
       : StatefulProcessor<__arityIn, __arityOut, SettingsType>(
             initialSettings) {}
