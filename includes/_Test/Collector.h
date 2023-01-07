5581a81231fb5f70d321d43df86124ce138202b1
Robert Albus <hello@robertalbus.com>
2023-01-07 12:30:45 -0700

    add debugging printer to _Test::Collector

--- includes/_Test/Collector.h
+++ includes/_Test/Collector.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 #include "Graph.h"
 
@@ -14,6 +15,21 @@ public:
   }
 
   std::vector<Frame<__arity>> frames;
+
+  void showBetween(size_t fromIndex, size_t toIndex) {
+    // add this to the collector node to dump the buffer
+    printf("\n\n");
+    for (int i = fromIndex; i <= toIndex; i++) {
+      std::string output = "\n ";
+      output += std::to_string(i);
+      for (int j = 0; j < __arity; j++) {
+        output += " - ";
+        output += std::to_string(frames[i][j]);
+      }
+      printf(output.c_str());
+    }
+    printf("\n\n");
+  }
 
 private:
   Frame<__arity> tick(Frame<__arity> input) {
