20e8ae3ce814d25b1a9eee768079e2681649c0b9
Robert Albus <hello@robertalbus.com>
2023-01-21 19:46:58 -0700

    convert Clover into a static lib so that it can be included as a git submodule dependency in other repos

---
+++ includes/Util/Calc.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include "Calc.h"
+
+namespace Clover::Util::Calc {
+
+float sign(float value) { return value < 0. ? -1 : 1; }
+
+float ltodb(float x) { return log10(fabs(x)) * 20.; }
+
+float dbtol(float x) { return powf32(10., x / 20.); }
+
+float mtof(float x) { return 440. * powf32(2., (x - 69.) / 12.); }
+
+float ftom(float x) { return 12. * (log(x / 220.) / log(2)) + 57.; }
+
+} // namespace Clover::Util::Calc
--- includes/Util/Calc.h
+++ includes/Util/Calc.h
@@ -4,14 +4,14 @@
 
 namespace Clover::Util::Calc {
 
-inline float sign(float value) { return value < 0. ? -1 : 1; }
+float sign(float value);
 
-inline float ltodb(float x) { return log10(fabs(x)) * 20.; }
+float ltodb(float x);
 
-inline float dbtol(float x) { return powf32(10., x / 20.); }
+float dbtol(float x);
 
-inline float mtof(float x) { return 440. * powf32(2., (x - 69.) / 12.); }
+float mtof(float x);
 
-inline float ftom(float x) { return 12. * (log(x / 220.) / log(2)) + 57.; }
+float ftom(float x);
 
 } // namespace Clover::Util::Calc
