7e1f9fbaa364146c38badaa14d15c1bdb2066711
Robert Albus <hello@robertalbus.com>
2023-01-25 23:27:56 -0700

    upgrade NullAdapter to StatelessProcessorV2

--- includes/NodeSimplex/Adapter/NullAdapter.h
+++ includes/NodeSimplex/Adapter/NullAdapter.h
@@ -3,10 +3,10 @@
 namespace Clover::NodeSimplex::Adapter {
 
 template <size_t __arityIn, size_t __arityOut>
-auto adapterFn = [](Frame<__arityIn> input) { return Frame<__arityOut>{}; };
-template <size_t __arityIn, size_t __arityOut>
-class NullAdapter
-    : public StatelessProcessor<__arityIn, __arityOut,
-                                adapterFn<__arityIn, __arityOut>> {};
+class NullAdapter : public StatelessProcessorV2<__arityIn, __arityOut> {
+  Frame<__arityOut> processFn(Frame<__arityIn> input) {
+    return Frame<__arityOut>{};
+  }
+};
 
 } // namespace Clover::NodeSimplex::Adapter
