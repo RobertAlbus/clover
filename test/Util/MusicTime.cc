276b7be6185aa11c0a17215bc199336c1b4f1b82
Robert Albus <hello@robertalbus.com>
2023-01-25 20:58:33 -0700

    update test for Time to be valid even for non-uniform (bpm/sample rate) combos

--- test/Util/MusicTime.cc
+++ test/Util/MusicTime.cc
@@ -3,20 +3,23 @@
 #include "Clover.h"
 
 TEST(Util_MusicTime, ShouldProvideCorrectTime) {
-    float sampleRate = 48000.0;
-    float minute = 60. * sampleRate;
-    float bpm = 160.;
-    
-    float beatLength = minute / bpm;
-    
-    SampleClock clock;
-    Time time(bpm, (int)sampleRate, &clock);
+  float sampleRate = 48000.0;
+  float minute = 60. * sampleRate;
+  float bpm = 159.;
 
-    int two_and_a_half = (int) (beatLength * 2.5);
-    for (int i = 0; i < two_and_a_half; i++) {
-        clock.tick();
-    }
+  float beatLength = minute / bpm;
+
+  SampleClock clock;
+  Time time(bpm, (int)sampleRate, &clock);
+
+  int duration_two_and_a_half_beats = (int)(beatLength * 2.5);
+  for (int i = 0; i < duration_two_and_a_half_beats; i++) {
+    clock.tick();
+  }
 
   EXPECT_EQ(time.beat(), beatLength);
-    EXPECT_EQ(time.currentBeat_i(), 2);
+  EXPECT_EQ((float)time.beat_i(), (int)beatLength);
+  EXPECT_EQ(time.currentBeat(),
+            ((float)duration_two_and_a_half_beats / time.beat()));
+  EXPECT_EQ(time.currentBeat_i(), 2);
 }
