ec3c144799caa0045832ef4527d9b0904e9933c6
Robert Albus <hello@robertalbus.com>
2023-02-18 22:22:16 -0700

    create a test to validate Node input summation and output gain

--- test/Graph/Node_Test.cc
+++ test/Graph/Node_Test.cc
@@ -28,3 +28,25 @@ TEST(Graph_Node, ShouldConnect) {
 
   EXPECT_EXIT(stub2 >> stub1, testing::ExitedWithCode(1), "");
 }
+
+TEST(Graph_Node, ShouldSumAndGain) {
+
+  Clover::_Test::HandCrank<1> crank;
+  Clover::_Test::Collector<1> collector(1);
+
+  NodeStub stub1;
+  NodeStub stub2;
+  NodeStub stub3;
+
+  stub1 >> collector >> crank;
+
+  stub2 >> stub1;
+  stub3 >> stub1;
+
+  crank.turn(1);
+  stub1.gain(2);
+  crank.turn(1);
+
+  EXPECT_EQ(collector.frames[0][0], 3.f);
+  EXPECT_EQ(collector.frames[1][0], 6.f);
+}
