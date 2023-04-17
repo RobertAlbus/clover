fa91f82420a585e3f7159759f4c6375828097ad6
Robert Albus <hello@robertalbus.com>
2023-04-16 23:58:37 -0600

    add functions for generating coefficients for a resonant butterworth filter.

--- includes/Algo/Filter/IIR/IIR_Coefficients.h
+++ includes/Algo/Filter/IIR/IIR_Coefficients.h
@@ -10,3 +10,14 @@ template <FloatingPoint T> struct IIRFilterCoefficients {
   T a1;
   T a2;
 };
+
+template <FloatingPoint T>
+void NormalizeIIRFilterCoefficients(T &a0, T &b0, T &b1, T &b2, T &a1, T &a2) {
+    T a0_inv = 1.0 / a0;
+    b0 *= a0_inv;
+    b1 *= a0_inv;
+    b2 *= a0_inv;
+    a1 *= a0_inv;
+    a2 *= a0_inv;
+    a0 = 1.0;
+}
