d89f4643d1104c0371f1e231228ed8ffbeafa0b5
Robert Albus <hello@robertalbus.com>
2023-06-09 17:52:31 -0600

    Make the arithmetic operators on Frame not return a value

--- includes/Graph/Frame.h
+++ includes/Graph/Frame.h
@@ -14,32 +14,26 @@ using FrameBuffer = std::array<Frame<__arity>, size>;
 /// add right frame to left frame.
 ///
 template <size_t __arity>
-Frame<__arity> &operator+=(Frame<__arity> &frame1,
+void operator+=(Frame<__arity> &frame1,
                            const Frame<__arity> &frame2) {
   for (int i = 0; i < __arity; i++) {
     frame1[i] += frame2[i];
   }
-
-  return frame1;
 }
 
 template <size_t __arity>
-Frame<__arity> &operator*=(Frame<__arity> &frame, float gain) {
+void operator*=(Frame<__arity> &frame, float gain) {
   for (int i = 0; i < __arity; i++) {
     frame[i] *= gain;
   }
-
-  return frame;
 }
 
 template <size_t __arity>
-Frame<__arity> &operator*=(Frame<__arity> &frame1,
+void operator*=(Frame<__arity> &frame1,
                            const Frame<__arity> &frame2) {
   for (int i = 0; i < __arity; i++) {
     frame1[i] *= frame2[i];
   }
-
-  return frame1;
 }
 
 } // namespace Clover::Graph
