41c1147b7b82db5446fe8b06822781960f0d376d
Robert Albus <hello@robertalbus.com>
2023-06-09 21:38:53 -0600

    Update Valgrind script sampling frequency

--- scripts/profileGetValgrind.sh
+++ scripts/profileGetValgrind.sh
@@ -3,5 +3,5 @@
 rm -fr ./build/CloverDemo;
 cmake -B build -G "Unix Makefiles" -DPROFILING_MODE=true;
 make -C build;
-LD_PRELOAD=/usr/local/lib/libprofiler.so CPUPROFILE=./test.prof ./build/CloverDemo && \
+LD_PRELOAD=/usr/local/lib/prof.so CPUPROFILE_FREQUENCY=48000 CPUPROFILE=./test.prof ./build/CloverDemo && \
 pprof -web -cum ./build/CloverDemo ./test.prof
