1d5ef073697abc1ebfb8e880346d4bd8361808b0
Robert Albus <hello@robertalbus.com>
2023-06-17 13:50:58 -0600

    Clean up the AudioFrame

--- includes/Graph/AudioFrame.h
+++ includes/Graph/AudioFrame.h
@@ -51,7 +51,7 @@ template <size_t __arity> struct AudioFrame {
   }
 
   AudioFrame<__arity> operator/(const AudioFrame<__arity>& b) const {
-        AudioFrame<__arity> f;
+    AudioFrame<__arity> f;
     for (int i = 0; i < __arity; i++) {
       f[i] = data[i] / b[i];
     }
@@ -67,7 +67,6 @@ template <size_t __arity> struct AudioFrame {
   void operator-=(const AudioFrame<__arity>& b) {
     for (int i = 0; i < __arity; i++) {
       data[i] -= b[i];
-
     }
   }
 
@@ -115,36 +114,5 @@ template <size_t __arity> struct AudioFrame {
 
   std::array<Sample, __arity> data;
 };
-
-
-
-
-template <size_t __arity, size_t size>
-using FrameBuffer = std::array<AudioFrame<__arity>, size>;
-
-/// add right frame to left frame.
-///
-// template <size_t __arity>
-// void operator+=(Frame<__arity> &frame1,
-//                            const Frame<__arity> &frame2) {
-//   for (int i = 0; i < __arity; i++) {
-//     frame1[i] += frame2[i];
-//   }
-// }
-
-// template <size_t __arity>
-// void operator*=(Frame<__arity> &frame, float gain) {
-//   for (int i = 0; i < __arity; i++) {
-//     frame[i] *= gain;
-//   }
-// }
-
-// template <size_t __arity>
-// void operator*=(Frame<__arity> &frame1,
-//                            const Frame<__arity> &frame2) {
-//   for (int i = 0; i < __arity; i++) {
-//     frame1[i] *= frame2[i];
-//   }
-// }
 
 } // namespace Clover::Graph
