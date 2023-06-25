cd4c0c71090b99d552dde8ecc196ffe400de1efb
Robert Albus <hello@robertalbus.com>
2023-06-25 15:49:11 -0600

    Remove process signature from the AlgorithmBase class
    Algorithms may not necessarily require input when process() is called, so adding a type-generic signature is not possible. This odea makes more sense on Nodes, where the IO is managed automatically by the Graph.

--- includes/Algo/AlgorithmBase.h
+++ includes/Algo/AlgorithmBase.h
@@ -1,12 +1,10 @@
 #pragma once
 
-template <typename T_in, typename T_out> struct AlgorithmBase {
+template <typename T> struct AlgorithmBase {
   AlgorithmBase() : processed() {}
 
-  virtual T_out process(const T_in &input) = 0;
-
-  const T_out &last() const { return processed; }
+  const T &last() const { return processed; }
 
 protected:
-  T_out processed;
+  T processed;
 };
