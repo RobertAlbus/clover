deb02a2ad0eb84ebbd255cbbef0d7644d5b8e998
Robert Albus <hello@robertalbus.com>
2023-08-20 20:33:01 -0600

    Prepend GPL V3 license statement to the beginning of source files

--- includes/Clover.h
+++ includes/Clover.h
@@ -1,5 +1,26 @@
 #pragma once
 
+/*
+ * /////////
+ * // Clover
+ * 
+ * Audio processing algorithms and DAG with feedback loops that do not break
+ * acyclicity.
+ * 
+ * Copyright (C) 2023 Rob W. Albus
+ *  
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+
 #include "Algorithm.h"
 #include "Base.h"
 #include "Config.h"
