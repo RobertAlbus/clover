ef35188f76724e27c90a65d57e1a246a251afba0
Robert Albus <hello@robertalbus.com>
2023-09-13 16:11:05 -0600

    Convert call graph profiling script to a CMake target

--- scripts/profileGetValgrind.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -fr ./build/CloverDemo;
-cmake -B build -G "Unix Makefiles" -DPROFILING_MODE=true;
-make -C build;
-LD_PRELOAD=/usr/local/lib/prof.so CPUPROFILE_FREQUENCY=48000 CPUPROFILE=./test.prof ./build/CloverDemo && \
-pprof -web -cum ./build/CloverDemo ./test.prof
