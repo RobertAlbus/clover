dc6ae09378a4cc4ea23f2755f524e0c42af5e9c4
Robert Albus <hello@robertalbus.com>
2023-11-18 17:53:28 -0700

    Add a semaphore to the main thread to sync groups of workers before creating more workers in order to simulate the modified topo sort that will be used for graph parallelism

--- src/experiments/persistentWorkers.cpp
+++ src/experiments/persistentWorkers.cpp
@@ -5,25 +5,28 @@
 #include <memory>
 #include <semaphore>
 
-int effort_ms = 100;
+int effort = 10;
 int task_count = 100;
+int worker_count = 20;
+
+std::counting_semaphore<> completedWorkSemaphore(0);
 
 
 class ThreadPool {
 public:
-    ThreadPool(size_t numThreads) : done(false), semaphore(0) {
+    ThreadPool(size_t numThreads) : done(false), newWorkSemaphore(0) {
         for (size_t i = 0; i < numThreads; ++i) {
             workers.emplace_back([this, i] {
                 printf("\nstarting: %i", i);
                 while (true) {
-                    semaphore.acquire();
+                    newWorkSemaphore.acquire();
                     if (done) {
                         printf("\ndone: %i", i);
                         break;
                     }
-                    std::this_thread::sleep_for(std::chrono::milliseconds(effort_ms));
-                    
                     printf("\nworking: %i", i);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(effort));
+                    completedWorkSemaphore.release();
                 }
             });
         }
@@ -32,34 +35,59 @@ public:
     ~ThreadPool() {
         done = true;
         for (size_t i = 0; i < workers.size(); ++i) {
-            semaphore.release();
+            newWorkSemaphore.release();
         }
         for (auto& worker : workers) {
             if (worker.joinable()) {
                 worker.join();
             }
         }
+        printf("\n");
     }
 
     void enqueue() {
-        semaphore.release();
+        newWorkSemaphore.release();
     }
 
 private:
     std::vector<std::thread> workers;
     std::atomic<bool> done;
-    std::counting_semaphore<> semaphore;
+    std::counting_semaphore<> newWorkSemaphore;
 };
 
 int main() {
-    ThreadPool pool(4);
+    ThreadPool pool(worker_count);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    for (int i = 0; i < task_count; ++i) {
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    printf("\nbatch 1 ----------------");
+    int batch_size_1 = task_count / 2;
+    for (int i = 0; i < batch_size_1; ++i) {
         pool.enqueue();
     }
+    for (int i = 0; i < batch_size_1; ++i) {
+        completedWorkSemaphore.acquire();
+    }
 
-    std::this_thread::sleep_for(std::chrono::seconds(4));
+    printf("\nbatch 2 ----------------");
+    int batch_size_2 = task_count - batch_size_1;
+    for (int i = 0; i < batch_size_2; ++i) {
+        pool.enqueue();
+    }
+    for (int i = 0; i < batch_size_2; ++i) {
+        completedWorkSemaphore.acquire();
+    }
 
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    printf("\n");
+    printf("\neffort time:  %i milliseconds", effort * task_count);
+    printf("\nactual time:  %i milliseconds", duration);
+    printf("\neffort saved: %i%%", (int)(100.f - ((float)duration) / ((float)effort * task_count) * 100.f));
+    printf("\n");
 
     return 0;
 }
