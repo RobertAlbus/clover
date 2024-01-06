fdde7db1380cc8041cb3278bd6553984b6a43688
Robert Albus <hello@robertalbus.com>
2024-01-06 11:00:55 -0700

    Remove unused and incorrect IIR filter coefficient algorithms

--- includes/Algorithm.h
+++ includes/Algorithm.h
@@ -26,9 +26,6 @@
 
 #include "Algo/DynamicRange/AsymClip.h"
 
-#include "Algo/Filter/IIR/ButterworthCoefficients.h"
-#include "Algo/Filter/IIR/ChebyshevType1Coefficients.h"
-#include "Algo/Filter/IIR/ChebyshevType2Coefficients.h"
 #include "Algo/Filter/IIR/IIRFilterDF2T.h"
 #include "Algo/Filter/IIR/IIR_Coefficients.h"
 #include "Algo/Filter/IIR/RbjBiquadCoefficients.h"
