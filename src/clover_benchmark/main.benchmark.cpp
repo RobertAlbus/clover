deca69a4d23f6f0424ceea7a19946e0e9b5148fe
Robert Albus <hello@robertalbus.com>
2024-11-24 12:51:32 -0700

    Update license to GPL V3

--- src/clover_benchmark/main.benchmark.cpp
+++ src/clover_benchmark/main.benchmark.cpp
@@ -1,3 +1,6 @@
+// This file is part of Clover Audio Framework, Copyright (C) 2023  Rob W. Albus
+// It is licensed under the GPLv3. See LICENSE for details.
+
 #include "benchmark/benchmark.h"
 
 #include "clover/example.hpp"
--- src/clover_lib/clover/example.hpp
+++ src/clover_lib/clover/example.hpp
@@ -1,3 +1,6 @@
+// This file is part of Clover Audio Framework, Copyright (C) 2023  Rob W. Albus
+// It is licensed under the GPLv3. See LICENSE for details.
+
 #pragma once
 
 namespace clover {
