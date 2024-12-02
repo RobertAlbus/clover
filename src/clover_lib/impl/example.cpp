8a9cdb41efc52e687145f394a3a18986a3135675
Rob Albus <hello@robertalbus.com>
2024-12-01 22:18:17 -0700

    Remove example code from original repo boilerplate

--- src/clover_lib/impl/example.cpp
+++
@@ -1,6 +0,0 @@
-// This file is part of Clover Audio Framework, Copyright (C) 2023  Rob W. Albus
-// It is licensed under the GPLv3. See LICENSE for details.
-
-#include "clover/example.hpp"
-
-auto clover::example() -> int { return 0; }
--- src/clover_test/main.test.cpp
+++
@@ -1,8 +0,0 @@
-// This file is part of Clover Audio Framework, Copyright (C) 2023  Rob W. Albus
-// It is licensed under the GPLv3. See LICENSE for details.
-
-#include <gtest/gtest.h>
-
-#include "clover/example.hpp"
-
-TEST(Example, ExampleTest) { EXPECT_EQ(clover::example(), 0); }
