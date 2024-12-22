e1124c13c1e6538ac5fc21f317c75aea13c650cc
Rob Albus <hello@robertalbus.com>
2024-12-21 22:34:47 -0700

    include both streaming and file rendering in the demo

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -4,23 +4,33 @@
 
 #include "clover/dsp/oscillator.hpp"
 #include "clover/io/audio_callback.hpp"
+#include "clover/io/audio_file.hpp"
 #include "clover/io/stream.hpp"
 #include "clover/io/system_audio.hpp"
 #include "clover/math.hpp"
 
-using namespace clover;
+bool PLAYBACK = false;
+bool RENDER   = true;
 
+using namespace clover;
 int main(int argc, char *argv[]) {
-    float fs     = 48000;
-    int fs_i     = static_cast<int>(fs);
-    int duration = 5 * fs_i;
+    float fs              = 48000;
+    int fs_i              = static_cast<int>(fs);
+    int duration          = 5 * fs_i;
+    int channel_count_out = 2;
 
     dsp::oscillator osc(fs);
-    osc.freq(400);
     dsp::oscillator mod(fs);
-    mod.freq(2);
 
-    auto audioCallback = [&](io::callback_args data) {
+    auto audio_state_init = [&]() {
+        osc.freq(400);
+        mod.freq(2);
+        osc.phase(0);
+        mod.phase(0);
+    };
+    audio_state_init();
+
+    auto audio_callback = [&](io::callback_args data) {
         float &L = *(data.output);
         float &R = *(data.output + 1);
 
@@ -36,24 +46,29 @@ int main(int argc, char *argv[]) {
         return io::callback_status::cont;
     };
 
-    io::stream stream;
-    io::system_audio_config system;
+    if (PLAYBACK) {
+        io::system_audio_config system;
+        system.print();
+        io::stream stream;
+        stream.audio_callback = audio_callback;
 
-    system.print();
+        stream.open(io::stream::settings{
+                .device_index_in  = system.no_device(),
+                .chan_count_in    = 0,
+                .device_index_out = system.default_output().index,
+                .chan_count_out   = channel_count_out,
+                .sample_rate      = fs_i,
+                .latency_ms       = 0});
 
-    stream.open(io::stream::settings{
-            .device_index_in  = system.no_device(),
-            .chan_count_in    = 0,
-            .device_index_out = system.default_output().index,
-            .chan_count_out   = 2,
-            .sample_rate      = fs_i,
-            .latency_ms       = 0});
+        stream.start();
+        stream.wait_to_complete();
+        stream.stop();
+    }
 
-    stream.audio_callback = audioCallback;
-
-    stream.start();
-    stream.wait_to_complete();
-    stream.stop();
-
-    return 0;
+    if (RENDER) {
+        audio_state_init();
+        audio_buffer render = io::exec_callback(audio_callback, channel_count_out, fs_i, duration);
+        render.sample_rate  = fs_i;
+        io::audio_file::write("test.wav", render, io::audio_file_settings::wav_441_16);
+    }
 }
