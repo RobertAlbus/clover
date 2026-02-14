511484f8c012accfcaea40a421b223b2243a3c0d
Rob Albus <hello@robertalbus.com>
2026-02-13 22:42:33 -0700

    skip benchmark assertions when filter applied

--- src/clover_lib/clover/main.benchmark.cpp
+++ src/clover_lib/clover/main.benchmark.cpp
@@ -1,10 +1,19 @@
 #include <benchmark/benchmark.h>
+#include <cstring>
 #include <gtest/gtest.h>
 #include <iostream>
 
 #include "clover/clover_benchmark/intercept_reporter.hpp"
 
 int main(int argc, char** argv) {
+    bool has_bm_filter = false;
+    for (int i = 1; i < argc; ++i) {
+        if (std::strncmp(argv[i], "--benchmark_filter", 18) == 0) {
+            has_bm_filter = true;
+            break;
+        }
+    }
+
     // benchmark calls exit(0) after printing --help, so initialize it second
     testing::InitGoogleTest(&argc, argv);
     benchmark::Initialize(&argc, argv);
@@ -15,6 +24,11 @@ int main(int argc, char** argv) {
     InterceptReporter reporter;
     benchmark::RunSpecifiedBenchmarks(&reporter);
     benchmark::Shutdown();
+
+    if (has_bm_filter) {
+        std::cout << "\nskipping performance assertions (benchmark filter active)\n";
+        return 0;
+    }
 
     std::cout << "\n---------------------------------------------------------------------\n\n\n";
 
